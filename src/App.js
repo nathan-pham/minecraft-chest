4bf86e4789c9fddeaaba4587515e8bd64a282965
Nathan Pham <nathanpham.me@gmail.com>
2021-06-08 16:38:04 -0700

    floor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { softShadows } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import React, { Suspense } from "react"
 
 import Lights from "./components/three/Lights"
 import Chest from "./components/three/Chest"
+import Floor from "./components/three/Floor"
+
+softShadows()
 
 export default function App() {
   return (
     <>
-      <Canvas colorMangement shadowMap camera={{position: [-5, 4, 4], fov: 40}}>
+      <Canvas colorManagement shadowMap camera={{ position: [-5, 4, 4], fov: 40 }}>
         <Lights />
         <Suspense fallback={null}>
           <Chest />
+          <Floor />
         </Suspense>
       </Canvas>
     </>
--- src/components/three/Lights.js
+++ src/components/three/Lights.js
@@ -3,21 +3,21 @@ import React from "react"
 export default function Lights() {
     return (
         <>
-            <fog attach="fog" args={["#fff", 0, 22]} />
+            <fog attach='fog' args={["#fff", 0, 22]} />
             <ambientLight intensity={0.4} />
-            <directionalLight 
-                castShadow 
-                position={[-8, 16, -8]} 
+            <directionalLight
+                castShadow
+                position={[-8, 16, -8]}
                 intensity={0}
                 shadow-mapSize-width={1024}
                 shadow-mapSize-height={1024}
                 shadow-camera-far={50}
                 shadow-camera-left={-10}
-                shadow-camera-right={10} 
-                shadow-camera-top={10} 
-                shadow-camera-bottom={-10} 
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
             />
-            <pointLight position={ [0, 50, 0] } intensity={ 2 } />
+            <pointLight position={[0, 50, 0]} intensity={2} />
         </>
     )    
 }
