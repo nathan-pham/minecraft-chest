c3b4de2da5c6557b7e872905a4ce7d7f9f93affc
Nathan Pham <nathanpham.me@gmail.com>
2021-06-08 16:13:34 -0700

    fixed styles & chest path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Canvas } from "react-three-fiber"
+import React, { Suspense } from "react"
+
+import Model from "./components/three/Chest"
+
+const cameraSettings = { position: [-5, 4, 4], fov: 40 }
+
 export default function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <Canvas colorMangement shadowMap camera={ cameraSettings }>
+        <Suspense fallback={ null }>
+          <Model />
+        </Suspense>
+      </Canvas>
+    </>
   )
 }
