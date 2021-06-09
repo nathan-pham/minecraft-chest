0ac8a684ae793718736e3e517ed6bca718f79a0b
Nathan Pham <nathanpham.me@gmail.com>
2021-06-08 18:15:39 -0700

    orbit controls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { softShadows, Loader, OrbitControls } from "@react-three/drei"
+import { Canvas, useThree } from "@react-three/fiber"
 import React, { Suspense, useState } from "react"
-import { softShadows } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { useSpring } from "@react-spring/core"
 
 import Lights from "./components/three/Lights"
 import Chest from "./components/three/Chest"
 import Floor from "./components/three/Floor"
 
 softShadows()
+
+const ZoomOrbital = () => {
+  const {gl, camera} = useThree()
+
+  useSpring({
+    from: { z: 30 },
+    x: -5,
+    y: 4,
+    z: 4,
+
+    onFrame: ({x, y, z}) => {
+      camera.position.set(x, y, z)
+    }
+  })
+
+  return (
+    <OrbitControls enableZoom={false} enablePan={false} target={[0, 0, 0]} args={[camera, gl.domElement]}/>
+  )
+}
 
 export default function App() {
   const [open, setOpen] = useState(false)
@@ -18,8 +38,10 @@ export default function App() {
         <Suspense fallback={null}>
           <Chest open={open} setOpen={setOpen} />
           <Floor />
+          <ZoomOrbital />
         </Suspense>
       </Canvas>
+      <Loader />
     </>
   )
 }
