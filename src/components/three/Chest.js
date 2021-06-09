5543c9ce2b0465bf653058afbd309e9a71636fd8
Nathan Pham <nathanpham.me@gmail.com>
2021-06-08 18:10:04 -0700

    open sounds

--- src/components/three/Chest.js
+++ src/components/three/Chest.js
@@ -2,13 +2,24 @@ import { useSpring, a } from "@react-spring/three"
 import { useGLTF } from "@react-three/drei"
 import React, { useRef } from "react"
 
+import _chestCloseSound from "../../assets/sound/chestClose.mp3"
+import _chestOpenSound from "../../assets/sound/chestOpen.mp3"
+
+const chestCloseSound = new Audio(_chestCloseSound)
+const chestOpenSound = new Audio(_chestOpenSound)
 const modelPath = "../../../coffre/model.glb"
+
+chestCloseSound.volume = 0.3
+chestOpenSound.volume = 0.3
 
 export default function Chest({ open, setOpen }) {
   const group = useRef()
   const { nodes, materials } = useGLTF(modelPath)
 
-  const handleOpen = () => setOpen(!open) 
+  const handleOpen = () => {
+    open ? chestCloseSound.play() : chestOpenSound.play()
+    setOpen(!open)
+  }
 
   const openAnimation = useSpring({
     rotation: open ? [0, 0, 0] : [1.61, 0, 0],
